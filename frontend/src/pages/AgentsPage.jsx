5969748792166c75072f38fc11ace5f551983c9e
Roshan Singh <roshan20031112@gmail.com>
2026-04-07 09:25:37 +0530

    made file structure

---
+++ frontend/src/pages/AgentsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AgentsPage = () => {
+  return (
+    <div>
+      agent page
+    </div>
+  )
+}
+
+export default AgentsPage
---
+++ frontend/src/pages/DebatePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DebatePage = () => {
+  return (
+    <div>
+      debate page
+    </div>
+  )
+}
+
+export default DebatePage
